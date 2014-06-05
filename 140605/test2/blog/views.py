94742220268ab9a7c6c0dc9e9a57b18a0296062d
bolpigo <yinjincao@gmail.com>
2014-06-05 10:18:58 +0800

    add python web django test2 html

---
+++ 140605/test2/blog/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader,Context
+from django.shortcuts import render_to_response
+# Create your views here.
+'''
+def index(req):
+	t = loader.get_template('index1.html')
+	c = Context({})
+	return HttpResponse(t.render(c))
+'''
+def index(req):
+	return render_to_response('index1.html',{})
